d62317ce7329ab61e4b905f99a667e7ba697d591
lma2023 <lyassovam@gmail.com>
2023-08-02 14:12:54 -0700

    Sending HTML

--- express_server.js
+++ express_server.js
@@ -16,3 +16,6 @@ app.get("/urls.json", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 })
+app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  });
