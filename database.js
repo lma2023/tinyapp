69b804a646d3b775c62206c127513c7f6799d0cd
lma2023 <lyassovam@gmail.com>
2023-09-11 19:28:43 -0700

    New database file crated to store databases

---
+++ database.js
@@ -0,0 +1,26 @@
+// Databases for the project
+const users = {
+    userRandomID: {
+      id: "userRandomID",
+      email: "user@example.com",
+      password: "purple-monkey-dinosaur",
+    },
+    user2RandomID: {
+      id: "user2RandomID",
+      email: "user2@example.com",
+      password: "dishwasher-funk",
+    },
+  };
+  
+  const urlDatabase = {
+    b6UTxQ: {
+      longURL: "https://www.tsn.ca",
+      user_id: "aJ48lW",
+    },
+    i3BoGr: {
+      longURL: "https://www.google.ca",
+      user_id: "aJ48lW",
+    },
+  };
+  
+  module.exports = { users, urlDatabase };
